712c056175afb336a8863d5796567f7069fe3d30
Luis Paolini <luis.paolini@gmail.com>
2022-04-09 10:50:50 -0300

    First lambda

---
+++ src/main/java/org/mbda/SimpleLambda.java
@@ -0,0 +1,15 @@
+package org.mbda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class SimpleLambda implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+
+        String message = System.getenv("MESSAGE");
+
+        return message + ": " + input;
+    }
+}
